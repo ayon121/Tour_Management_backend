ade4badc4523af27917c427db67806d58efd431e
ayon121 <borshonsaha1234@gmail.com>
2025-07-12 16:20:17 +0600

    route optimization completed

--- src/app/Modules/user/user.controller.ts
+++ src/app/Modules/user/user.controller.ts
@@ -1,15 +1,10 @@
 import { Request, Response } from "express";
-import { User } from "./user.model";
+import { UserServices } from "./user.service";
 
 
 const createUser = async (req : Request , res : Response) => {
     try {
-        const {name , email} = req.body;
-
-        const user = await User.create({
-            name : name ,
-            email : email,
-        })
+        const user = await UserServices.createUserService(req.body)
 
         res.status(201).json({
             message : "User Created Successfully",
@@ -18,6 +13,7 @@ const createUser = async (req : Request , res : Response) => {
         
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (err : any) {
+        // eslint-disable-next-line no-console
         console.log(err);
         res.status(400).json({
             message : `Something Went Wrong, Please Try Again Later ${err.message}`
