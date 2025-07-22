1c9570b1ea0f4a4ffbaa790fc3c065d911faff65
ayon121 <borshonsaha1234@gmail.com>
2025-07-23 02:54:12 +0600

    slug added

--- src/app/Modules/Devision/division.service.ts
+++ src/app/Modules/Devision/division.service.ts
@@ -3,21 +3,22 @@ import { Division } from "./division.model";
 
 const createDivision = async (payload: IDivision) => {
 
+
     const existingDivision = await Division.findOne({ name: payload.name });
     if (existingDivision) {
         throw new Error("A division with this name already exists.");
     }
 
 
-    // const baseSlug = payload.name.toLowerCase().split(" ").join("-")
-    // let slug = `${baseSlug}-division`
+    const baseSlug = payload.name.toLowerCase().split(" ").join("-")
+    let slug = `${baseSlug}-division`
 
-    // let counter = 0;
-    // while (await Division.exists({ slug })) {
-    //     slug = `${slug}-${counter++}` // dhaka-division-2
-    // }
+    let counter = 0;
+    while (await Division.exists({ slug })) {
+        slug = `${slug}-${counter++}` // dhaka-division-2
+    }
 
-    // payload.slug = slug;
+    payload.slug = slug;
 
     const division = await Division.create(payload);
 
